ba7b7d1afd8ded79cd61481177f2a1804580bad3
zslsun <96239042+zslsun@users.noreply.github.com>
2022-10-16 17:55:39 +0800

    pull commit

--- src/main/java/com/atguigu/GitTest.java
+++ src/main/java/com/atguigu/GitTest.java
@@ -11,5 +11,6 @@ public class GitTest {
         System.out.println("hello git3");
         System.out.println("hello git4");
         System.out.println("push test");
+        System.out.println("pull test");
     }
 }
